f51735bde7d4fba3591161b68f68d5cd189dac66
scripac <mukhammadiev013@gmail.com>
2022-04-15 00:36:39 +0500

    Module Profile (get)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { TagModule } from '@app/tag/tag.module'
 import { UserModule } from '@app/user/user.module'
 import { AuthMiddleware } from '@app/user/middlewares/auth.middleware'
 import { ArticleModule } from '@app/article/article.module'
+import { ProfileModule } from '@app/profile/profile.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ src/profile/profile.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProfileController } from '@app/profile/profile.controller'
+import { ProfileService } from '@app/profile/profile.service'
+import { UserEntity } from '@app/user/user.entity'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [ProfileController],
+  providers: [ProfileService]
+})
+export class ProfileModule {
+
+}
